1b77a47fa866a1c2975c8dda18789b08d6fc9eac
blanneau44 <49789284+blanneau44@users.noreply.github.com>
2020-07-24 17:41:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,3 +16,18 @@ requestButton.addEventListener('click', async () => {
     console.log('HID: ${device.productName}');
   }
 });
+      document.querySelector('#my-button').addEventListener('click', function(event) {
+        // Permissions must be requested from inside a user gesture, like a button's
+        // click handler.
+        chrome.permissions.request({
+          permissions: ['usb'],
+          origins: ['http://www.google.com/']
+        }, function(granted) {
+          // The callback argument will be true if the user granted the permissions.
+          if (granted) {
+            doSomething();
+          } else {
+            doSomethingElse();
+          }
+        });
+      });
