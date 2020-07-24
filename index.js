a633cc4d89aee2d1694606dd57df32c54f5f67e5
blanneau44 <49789284+blanneau44@users.noreply.github.com>
2020-07-24 17:03:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,3 @@
-/*
-document.getElementById('arduinoButton').addEventListener('click', function () {
-  if (chrome.hid) {
-    talkToArduino();
-    console.log('web HID not supported.');
-  } else {
-    console.log('WebUSB not supported.');
-  }
-});
-
-async function talkToArduino() {
-  console.log("GO");
-  try {
-    chrome.hid.getDevices()
-    .then(devices => {
-      console.log("Total devices: " + devices.length);
-      devices.forEach(device => {
-        console.log("Product name: " + device.productName + ", serial number " + device.serialNumber);
-      });
-    });
-  } catch (error) {
-    document.getElementById('target').innerHTML = "-->"+error;
-  }
-}
-*/
-
-/*const consentButton = document.getElementById('consent-button');
-
-const deviceFilters = [{ }];
-
-consentButton.addEventListener('click', async () => {
-  let device;
-
-  let devices = await navigator.hid.getDevices();
-  devices.forEach(device => {
-    console.log('HID: ${device.productName}');
-  });
-  */
-
-
 let requestButton = document.getElementById('request-hid-device');
 requestButton.addEventListener('click', async () => {
   let device;
@@ -55,19 +15,4 @@ requestButton.addEventListener('click', async () => {
   if (device !== undefined) {
     console.log('HID: ${device.productName}');
   }
-});
-
-
-
-
-
-  /*
-  try {
-    const devices = await navigator.hid.requestDevice({ filters: deviceFilters });
-    device = devices[0];
-  } catch (error) {
-    console.warn('No device access granted', error);
-    return;
-  }
-  */
 });
