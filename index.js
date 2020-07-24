9898ac866212bb33f8e474850e23c03522d0eb0f
blanneau44 <49789284+blanneau44@users.noreply.github.com>
2020-07-24 18:06:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,19 +16,17 @@ requestButton.addEventListener('click', async () => {
     console.log('HID: ${device.productName}');
   }
 });
-      document.getElementById('bouton01').addEventListener('click', function(event) {
-        console.log("OK");
-        // Permissions must be requested from inside a user gesture, like a button's
-        // click handler.
-        navigator.permissions.request({
-          permissions: ['usb'],
-          origins: ['https://blanneau44.github.io/']
-        }, function(granted) {
-          // The callback argument will be true if the user granted the permissions.
-          if (granted) {
-            console.log("OK1");
-          } else {
-            console.log("KO");
-          }
-        });
-      });
+document.querySelector('bouton01').addEventListener('click', function(event) {
+  // Permissions must be requested from inside a user gesture, like a button's
+  // click handler.
+  chrome.permissions.request({
+    permissions: ['usb'],
+  }, function(granted) {
+    // The callback argument will be true if the user granted the permissions.
+    if (granted) {
+      doSomething();
+    } else {
+      doSomethingElse();
+    }
+  });
+});
