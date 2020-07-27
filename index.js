a7795fc66426841b3d619a943c906d3755d23434
blanneau44 <49789284+blanneau44@users.noreply.github.com>
2020-07-27 09:11:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -56,6 +56,6 @@ function requestPermissions() {
   });
 }
 
-document.querySelector('bouton01').addEventListener("click", requestPermissions);
+document.getElementById('bouton01').addEventListener("click", requestPermissions);
 
 
