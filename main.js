5562ef9c8b899a212fcf3f81ab447d13906f5011
blanneau44 <49789284+blanneau44@users.noreply.github.com>
2020-07-27 11:18:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create(
-      "control-panel.html",
+      "index.html",
       {
         innerBounds: { width: 1060, height: 510, minWidth: 1060 }
       });
